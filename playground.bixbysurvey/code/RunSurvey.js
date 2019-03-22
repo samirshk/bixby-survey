13abecc7f033624d41f103f179b77dfa79735775
Samir Shaikh <s.shaikh@samsung.com>
2019-03-21 19:49:00 -0700

    bixby surveys - samir update

--- playground.bixbysurvey/code/RunSurvey.js
+++ playground.bixbysurvey/code/RunSurvey.js
@@ -1,15 +1,4 @@
-module.exports.function = function runSurvey (surveyNumber, surveyRatingText) {
-  var rating = 5;
-  if(surveyRatingText.includes("five") || surveyRatingText.includes("good"))
-    rating = 5;
-  if(surveyRatingText.includes("1") || surveyRatingText.includes("crappy"))
-    rating = 1;
-  
-  var runningValue = window.document.surveyNumber
-  var newValue = (currentValue + rating)/2; //just our rating system.
-  document.window.surveyNumber = newValue;
-  
-  return {
-    runningValue : document.window.surveyNumber
-  }
+module.exports.function = function runSurvey (SurveyNumber, SurveyRating) {
+
+  return "Recorded " + SurveyRating + " for " + SurveyNumber
 }
